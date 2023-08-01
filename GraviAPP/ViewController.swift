e199b62c572bc785d6d699d269bd192eebee9ed3
Hydroxios <hydroxios@gmail.com>
2023-08-01 05:14:19 +0200

    Initial Commit

--- GraviAPP/ViewController.swift
+++ GraviAPP/ViewController.swift
@@ -1,19 +1,48 @@
-//
-//  ViewController.swift
-//  GraviAPP
-//
-//  Created by Hydro on 31/07/2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    let menuView = UIView()
+    let menuWidth: CGFloat = 200.0
+    var isMenuVisible = false
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func makeHTTPRequest(_ sender: UIButton) {
+        // 1. Créer une URL pour la requête
+        guard let url = URL(string: "https://gravityclan.fr/api/bot/clanxp.php") else {
+            print("URL invalide.")
+            return
+        }
+        
+        // 2. Créer une URLSession
+        let session = URLSession.shared
+        
+        // 3. Créer une tâche de données pour la requête
+        let task = session.dataTask(with: url) { (data, response, error) in
+            // Vérifier s'il y a des erreurs
+            if let error = error {
+                print("Erreur de requête : \(error.localizedDescription)")
+                return
+            }
+            
+            // Vérifier si des données ont été reçues
+            guard let data = data else {
+                print("Aucune donnée reçue.")
+                return
+            }
+            
+            do {
+                let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+                print(jsonObject)
+            } catch {
+                print("Erreur lors du traitement des données JSON : \(error.localizedDescription)")
+            }
+        }
+        
+        // 4. Lancer la tâche de données pour effectuer la requête
+        task.resume()
+    }
 }
-
